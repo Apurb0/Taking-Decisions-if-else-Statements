9f5e1780b8e0959f1370495b1ff82b188edf8666
Apurbo Sarkar <50476519+Apurb0@users.noreply.github.com>
2024-01-17 19:26:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const populationBangladesh = 130;
+if (populationBangladesh > 33) {
+    console.log("Portugal's population is above average");
+} else {
+    const avgPopulation = 33 - populationBangladesh;
+    console.log(`Portugal's population is ${avgPopulation} million below average`)
+}
